ceec2c6938c0bfc72c1801d04e1a8118334580d1
wusamin <tatsusamimi@gmail.com>
2021-04-12 23:36:40 +0900

    add AddMinute.

--- calc.go
+++ calc.go
@@ -12,6 +12,11 @@ func AddHour(t time.Time, hour int) time.Time {
 	return t.Add(time.Duration(hour) * time.Hour)
 }
 
+// AddMinute adds minutes to time.Time.
+func AddMinute(t time.Time, minutes int) time.Time {
+	return t.Add(time.Duration(minutes) * time.Minute)
+}
+
 // AddSeconds adds seconds to time.Time.
 func AddSeconds(t time.Time, seconds int) time.Time {
 	return t.Add(time.Duration(seconds) * time.Second)
