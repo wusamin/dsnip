2d33fe33f77e25f95147f6ed9a1ac1ae00365965
wusamin <tatsusamimi@gmail.com>
2021-08-31 23:17:26 +0900

    add date format constant.

---
+++ const.go
@@ -0,0 +1,8 @@
+package dsnip
+
+const (
+	YYYYMMDD              = "20060102"
+	YYYYMMDDHHmmSS        = "20060102150405"
+	YYYYMMDDHHmmSS_Slash  = "2006/01/02 15:04:05"
+	YYYYMMDDHHmmSS_Hyphen = "2006-01-02 15:04:05"
+)
